33db1d948c5230c91e9bb5ad2042f9acc42593e5
mike freund <mfreundc@gmail.com>
2020-09-27 12:28:04 -0500

    calling 3dREMLfit in parallel, with forking fix typos R scripts runwise

---
+++ code/glms/runwise/3dREMLfit.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+
+wd=$(pwd)
+
+
+for session_i in ${!sessions[@]}; do
+
+	for run_i in ${!runs[@]}; do
+		
+		## define paths and names
+		sess=${sessions[$session_i]:0:3}  ## get short name
+		sess=${sess^}  ## Namecase
+		dir_stimts=${stimts}${subject}/INPUT_DATA/${task}/${sessions[$session_i]}
+		dir_out=${out}${subject}/RESULTS/${task}/${sessions[$session_i]}_${glm}_EVENTS_censored${suffix}_${runs[$run_i]}
+		name_img=${img}${subject}/INPUT_DATA/${task}/${sessions[$session_i]}/lpi_scale_blur4_tfMRI_${task}${sess}${runs[$run_i]}_${encoding_dir[$run_i]}.nii.gz
+		
+		cd ${dir_out}
+		
+		echo ${task} ${glm} ${run_i}
+
+		/usr/local/pkg/afni_18/3dREMLfit \
+		-matrix ${dir_out}/X_${runs[$run_i]}.xmat.1D \
+		-input ${name_img} \
+		-Rvar ${dir_out}/stats_var_${subject}_${runs[$run_i]}_REML.nii.gz \
+		-Rbuck ${dir_out}/STATS_${subject}_${runs[$run_i]}_REML.nii.gz \
+		-rwherr ${dir_out}/wherr_${subject}_${runs[$run_i]}_REML.nii.gz \
+		-rerrts ${dir_out}/errts_${subject}_${runs[$run_i]}_REML.nii.gz \
+		-GOFORIT \
+		-fout \
+		-tout \
+		-nobout \
+		-noFDR \
+		-verb
+
+	done
+
+done
+
+cd ${wd}
+
---
+++ code/glms/runwise/master_3dREMLfit.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+
+## get vars
+
+glms=(Cues CongruencySwitch ListLength Congruency fix-item)
+tasks=(Axcpt Cuedts Stern Stroop Stroop)
+suffices=(_shifted _shifted _shifted _shifted "")
+sessions=baseline
+subjects=132017
+runs=(1 2)
+encoding_dir=(AP PA)
+
+
+## directories
+
+stimts=/data/nil-external/ccp/freund/ub55/out/glms/
+out=/data/nil-external/ccp/freund/ub55/out/glms/
+img=/data/nil-bluearc/ccp-hcp/DMCC_ALL_BACKUPS/HCP_SUBJECTS_BACKUPS/fMRIPrep_AFNI_ANALYSIS/
+
+
+## fit
+
+
+for subject in ${subjects[@]}; do
+
+	echo ${subject}
+	
+	for glm_i in ${!glms[@]}; do
+	
+		glm=${glms[$glm_i]}
+		task=${tasks[$glm_i]}
+		suffix=${suffices[$glm_i]}
+		
+		source 3dREMLfit.sh
+
+	done
+
+done
+
+
