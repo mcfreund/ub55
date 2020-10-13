b224412d8da32f869d3370d586f92c7ad9aad2eb
mcfreund <m.freund@wustl.edu>
2020-10-12 19:07:00 -0500

    read and save betas as RDS

--- code/_funs.R
+++ code/_funs.R
@@ -1,5 +1,85 @@
-tidy_lmer <- function(x) {
-  coefs <- as.data.frame(coef(summary(x)))
-  coefs$term <- rownames(coefs)
-  dplyr::rename(coefs, estimate = "Estimate", se = "Std. Error", "statistic" = "t value", p.value = "Pr(>|t|)")
+# tidy_lmer <- function(x) {
+#   coefs <- as.data.frame(coef(summary(x)))
+#   coefs$term <- rownames(coefs)
+#   dplyr::rename(coefs, estimate = "Estimate", se = "Std. Error", "statistic" = "t value", p.value = "Pr(>|t|)")
+# }
+
+
+read_betas <- function(
+  .subjs,
+  .task,
+  .glm,
+  .dir
+) {
+  # .subjs = subjs; .task = "Axcpt"; .glm = "baseline_Cues_EVENTS_censored_shifted"; .dir = dir.analysis
+  
+  ## initialize array
+  
+  pick.a.file <- 
+    file.path(.dir, .subjs[1], "RESULTS",  .task, paste0(.glm, "_1"), paste0("STATS_", subjs[1], "_1_L_REML.func.gii"))
+  labs <- afni("3dinfo", paste0("-label ", pick.a.file))
+  labs <- unlist(strsplit(labs, "\\|"))
+  is.reg <- !grepl("Full|block|Tstat|Fstat", labs)
+  tab <- do.call(rbind, strsplit(gsub("_Coef", "", labs[is.reg]), "#"))
+  trs <- as.numeric(unique(tab[, 2])) + 1
+  regs <- unique(tab[, 1])
+  
+  
+  n.vertex <- 10242
+  n.tr <- length(trs)
+  n.reg <- length(regs)
+  n.subj <- length(subjs)
+  
+  betas <- array(
+    NA,
+    dim = c(n.vertex*2, n.reg, n.tr, n.subj, 2),
+    dimnames = list(vertex = NULL, reg = regs, tr = NULL, subj = subjs, run = c("run1", "run2"))
+  )
+  
+  vertex.inds <- cbind(L = 1:n.vertex, R = (n.vertex + 1):(n.vertex * 2))
+  
+  for (subj.i in seq_along(subjs)) {
+    # subj.i = 1
+    
+    for (run.i in 1:2) {
+      # run.i = 1
+      
+      for (hemi.i in c("L", "R")) {
+        # hemi.i = "L"
+        
+        inds <- vertex.inds[, hemi.i]
+        
+        fname <- file.path(
+          .dir, .subjs[subj.i], "RESULTS",  .task, paste0(.glm, "_", run.i),  
+          paste0("STATS_", subjs[subj.i], "_", run.i, "_", hemi.i, "_REML.func.gii")
+        )
+        
+        if (!file.exists(fname)) next
+        
+        B <- mikeutils::read_gifti2matrix(fname)[is.reg, ]
+        
+        is.ok.i <- isTRUE(all.equal(dim(B), c(n.reg * n.tr, n.vertex)))
+        if (!is.ok.i) stop("mismatched beta array")
+        
+        
+        for (reg.i in n.reg) {
+          
+          is.reg.i <- grepl(regs[reg.i], labs[is.reg])
+          B.reg.i <- t(B[is.reg.i, ])
+          
+          is.ok.ii <- isTRUE(all.equal(dim(betas[inds, reg.i, , subj.i, run.i]), dim(B.reg.i)))
+          if (!is.ok.ii) stop("mismatched regressor array")
+          
+          betas[inds, reg.i, , subj.i, run.i] <- B.reg.i
+          
+          }
+        
+      }
+
+    }
+      
+  }
+  
+  betas
+    
 }
---
+++ code/multivar_runwise/save_betas.R
@@ -0,0 +1,35 @@
+
+source(here::here("code", "_packages.R"))
+source(here("code", "read-behav.R"))
+source(here("code", "_vars.R"))
+source(here("code", "_atlases.R"))
+source(here("code", "_settings.R"))
+source(here("code", "_funs.R"))
+
+
+## input: subjs, task, glmname
+## output: RDS files
+
+dirs <- expand.grid(subj = subjs, task = tasks, session = "baseline", stringsAsFactors = FALSE)
+glminfo <- data.frame(
+  task = c("Axcpt", "Cuedts", "Stern", "Stroop", "Stroop"),
+  name.glm = c(
+    "baseline_Cues_EVENTS_censored_shifted", 
+    "baseline_CongruencySwitch_EVENTS_censored_shifted",
+    "baseline_ListLength_EVENTS_censored_shifted",
+    "baseline_Congruency_EVENTS_censored_shifted",
+    "baseline_fix-item_EVENTS_censored"
+  ),
+  stringsAsFactors = FALSE
+)
+glminfo <- as.data.table(glminfo)
+
+
+
+for (glm.i in seq_len(nrow(glminfo))) {
+  
+  betas.i <- read_betas(subjs, glminfo[glm.i]$task, glminfo[glm.i]$name.glm, dir.analysis )
+  saveRDS(betas.i, here("out", "glms", paste0("betas_", glminfo[glm.i]$task, "_", glminfo[glm.i]$name.glm,  ".RDS")))
+    
+}
+
