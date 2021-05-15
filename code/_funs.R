7415236819ca8bac64f807c66b3236067903b61a
mcfreund <mfreundc@gmail.com>
2021-05-15 14:07:02 -0500

    fun for reading dmcc 2prk surf betas

--- code/_funs.R
+++ code/_funs.R
@@ -85,6 +85,81 @@ read_betas <- function(
 }
 
 
+read_betas_dmcc <- function(
+  .subjs,
+  .task,
+  .glm,
+  .dir
+) {
+  # glm.i = 4; .subjs = subjs; .task = glminfo$task[glm.i]; .glm = glminfo$name.glm[glm.i]; .dir = dir.analysis
+  
+  ## initialize array
+  
+  pick.a.file <- 
+    file.path(.dir, .subjs[1], "SURFACE_RESULTS",  .task, paste0(.glm), paste0("STATS_", subjs[1], "_REML_L.func.gii"))
+  labs <- afni("3dinfo", paste0("-label ", pick.a.file))
+  labs <- unlist(strsplit(labs, "\\|"))
+  is.reg <- !grepl("Full|block|Tstat|Fstat", labs)
+  tab <- do.call(rbind, strsplit(gsub("_Coef", "", labs[is.reg]), "#"))
+  trs <- as.numeric(unique(tab[, 2])) + 1
+  regs <- unique(tab[, 1])
+  
+  n.vertex <- 10242
+  n.tr <- length(trs)
+  n.reg <- length(regs)
+  n.subj <- length(subjs)
+  
+  betas <- array(
+    NA,
+    dim = c(n.vertex*2, n.reg, n.tr, n.subj),
+    dimnames = list(vertex = NULL, reg = regs, tr = NULL, subj = subjs)
+  )
+  
+  vertex.inds <- cbind(L = 1:n.vertex, R = (n.vertex + 1):(n.vertex * 2))
+  
+  for (subj.i in seq_along(subjs)) {
+    # subj.i = 1; hemi.i = "L"
+    
+    for (hemi.i in c("L", "R")) {
+      # hemi.i = "R"
+      
+      inds <- vertex.inds[, hemi.i]
+      
+      fname <- file.path(
+        .dir, .subjs[subj.i], "SURFACE_RESULTS",  .task, paste0(.glm),  
+        paste0("STATS_", subjs[subj.i], "_REML_", hemi.i, ".func.gii")
+      )
+      
+      if (!file.exists(fname)) next
+      
+      B <- mikeutils::read_gifti2matrix(fname)[is.reg, ]
+      
+      is.ok.i <- isTRUE(all.equal(dim(B), c(n.reg * n.tr, n.vertex)))
+      if (!is.ok.i) stop("mismatched beta array")
+      
+      
+      for (reg.i in seq_len(n.reg)) {
+        # reg.i = 1
+        
+        is.reg.i <- grepl(paste0("^", regs[reg.i], "#"), labs[is.reg])
+        B.reg.i <- t(B[is.reg.i, ])
+        
+        is.ok.ii <- isTRUE(all.equal(dim(betas[inds, reg.i, , subj.i]), dim(B.reg.i)))
+        if (!is.ok.ii) stop("mismatched regressor array")
+        
+        betas[inds, reg.i, , subj.i] <- B.reg.i
+        
+      }
+      
+    }
+    
+  }
+  
+  betas
+  
+}
+
+
 
 read_coefs <- function(
   .subjs,
---
+++ code/multitask/jocn/1_est_simil_jocn_2trpk.R
@@ -0,0 +1,180 @@
+
+source(here::here("code", "_packages.R"))
+source(here("code", "read-behav.R"))
+source(here("code", "_vars.R"))
+source(here("code", "_atlases.R"))
+source(here("code", "_settings.R"))
+source(here("code", "_funs.R"))
+
+
+# subjs <- subjs[!subjs %in% c("432332", "")]
+n.vert <- 20484
+
+glminfo <- data.frame(
+  task = c("Axcpt", "Cuedts", "Stern", "Stroop"),
+  name.glm = c(
+    "baseline_Cues_EVENTS_censored_shifted",
+    "baseline_CongruencySwitch_EVENTS_censored_shifted",
+    "baseline_ListLength_EVENTS_censored_shifted",
+    "baseline_Congruency_EVENTS_censored_shifted"
+  ),
+  stringsAsFactors = FALSE
+)
+glminfo <- as.data.table(glminfo)
+hi = list(
+  Axcpt = "BX",
+  Cuedts = c("InConInc", "InConNoInc"),
+  Stern = "LL5RN",
+  Stroop = c("biasInCon", "PC50InCon")
+)
+lo = list(
+  Axcpt = "BY",
+  Cuedts = c("ConInc", "ConNoInc"),
+  Stern = "LL5NN",
+  Stroop = c("biasCon", "PC50Con")
+)
+
+n.iter <- length(subjs)
+pb <- progress_bar$new(
+  format = " running [:bar] :percent eta: :eta (elapsed: :elapsed)",
+  total = n.iter, clear = FALSE, width = 120
+)
+
+
+
+## wrangle betas ----
+(time.start <- Sys.time())
+
+
+conditions <- combo_paste(tasks, c("hi", "lo"))
+
+betas <- 
+  array(
+    NA,
+    dim = c(n.vert, length(conditions), length(subjs)),
+    dimnames = list(vertex = NULL, condition = conditions, subj = subjs)
+  )
+for (glm.i in seq_len(nrow(glminfo))) {
+  # glm.i = 1
+  
+  name.glm.i <- glminfo[glm.i]$name.glm
+  name.task.i <- glminfo[glm.i]$task
+  
+  
+  
+  
+  
+  ## read betas:
+  betas.i <- readRDS(
+    "/data/nil-bluearc/ccp-hcp/DMCC_ALL_BACKUPS/HCP_SUBJECTS_BACKUPS/fMRIPrep_AFNI_ANALYSIS", 
+    paste0("betas_dmcc_2trpk_", glminfo[glm.i]$task, "_", glminfo[glm.i]$name.glm,  ".RDS"))
+  )
+  ## remove subj with missing data, get target TRs:
+  betas.i <- betas.i[, , target.trs[[glm.i]], !dimnames(betas.i)$subj %in% "432332", ]
+  betas.i <- aperm(betas.i, c("vertex", "subj", "reg", "tr", "run"))
+  
+  betas.i.hi <- rowMeans(betas.i[, , hi[[glm.i]], , ], dims = 2) ## ave across TR, run, relevant regs
+  betas.i.lo <- rowMeans(betas.i[, , lo[[glm.i]], , ], dims = 2)
+  betas.ii <- abind(betas.i.hi, betas.i.lo, along = 0)
+  
+  # ## average across target TRs:
+  # betas.i <- abind(
+  #   apply(betas.i[, hi[[glm.i]], target.trs[[glm.i]], , ], c("vertex", "subj", "run"), mean),
+  #   apply(betas.i[, lo[[glm.i]], target.trs[[glm.i]], , ], c("vertex", "subj", "run"), mean),
+  #   along = 0
+  # )   ## condition, vertex, subj
+  # 
+  # betas.ii <- (betas.i[, , , "run1"] + betas.i[, , , "run2"]) / 2  ## average across run
+  
+  
+  ## reshape to match dim(betas):
+  
+  betas.ii <- aperm(betas.ii, c(2, 1, 3))
+  conditions.i <- combo_paste(name.task.i, c("hi", "lo"))  ## run varies slower
+  betas[, conditions.i, subjs] <- betas.ii
+  
+  
+}
+rm(betas.i)
+gc()
+
+
+
+
+
+
+## est simil ----
+
+
+simil <- 
+  array(
+    NA,
+    dim = c(length(conditions), length(conditions), length(parcellation$key), length(subjs)),
+    dimnames = list(.row = conditions, .col = conditions, parcel = parcellation$key, subj = subjs)
+  )
+
+univa <- 
+  array(
+    NA,
+    dim = c(length(conditions), length(conditions), length(parcellation$key), length(subjs)),
+    dimnames = list(.row = conditions, .col = conditions, parcel = parcellation$key, subj = subjs)
+  )
+
+unifo <- 
+  array(
+    NA,
+    dim = c(length(conditions), length(conditions), length(parcellation$key), length(subjs)),
+    dimnames = list(.row = conditions, .col = conditions, parcel = parcellation$key, subj = subjs)
+  )
+
+
+for (subj.i in seq_along(subjs))  {
+  # subj.i = 1
+  
+  res <- enlist(parcellation$key)
+  
+  
+  name.subj.i <- subjs[subj.i]
+  betas.subj.i <- betas[, , subj.i]
+  
+  
+  for (parcel.i in seq_along(parcellation$key)) {
+    # parcel.i = 20
+    
+    ## mask:
+    
+    is.parcel <- schaefer10k == parcel.i
+    betas.subj.parcel.i <- betas.subj.i[is.parcel, ]
+    
+    has.signal.all.conds <- !rowSums(abs(betas.subj.parcel.i) < .Machine$double.eps) > 0
+    if (sum(has.signal.all.conds) <= length(conditions)) stop(paste0("bad parcel: ", subj.i, " ", parcel.i))
+    
+    B <- betas.subj.parcel.i[has.signal.all.conds, ]
+    
+    
+    ## estimate similarity matrices:
+    
+    simil[, , parcel.i, subj.i] <- cor(B)
+    
+    univa[, , parcel.i, subj.i] <- tcrossprod(colMeans(B))
+    unifo[, , parcel.i, subj.i] <- tcrossprod(colSums(B^2))
+    
+  }
+  
+  pb$tick()  ## progress bar
+  
+  
+}
+
+
+
+## save ----
+
+if (!dir.exists(here("out", "multitask"))) dir.create(here("out", "multitask"))
+saveRDS(simil, here("out", "multitask", paste0("corr-biased_unpre_jocn.RDS")))
+saveRDS(univa, here("out", "multitask", paste0("mean-biased_unpre_jocn.RDS")))
+saveRDS(unifo, here("out", "multitask", paste0("ssq-biased_unpre_jocn.RDS")))
+
+
+
+(time.run <- Sys.time() - time.start)
